206e76fa1e6af1b5e87f2a97f9152ebf539f0fac
TheTridentGuy <aiden.bohlander@gmail.com>
2024-11-26 19:42:49 -0800

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from doctest import debug
+from fileinput import filename
+from typing import override
+
+import flask
+from flask import Flask, request, render_template
+from werkzeug.utils import redirect
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+app.run(host="localhost", port=8080, debug=True)
