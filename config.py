b6a74dd33932492734818f87460839416923d3d1
Aiden Bohlander <aiden.bohlander@gmail.com>
2025-08-19 23:59:14 -0700

    trying to fix nekosapi :/

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ DEBUG = True
 FILE_SERVE_PATH = "/home/aiden/Downloads"
 UPLOAD_PASS_HASH = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8"
 NEKO_API_ENDPOINTS = {
-    "": "https://api.nekosapi.com/v3/images/random?rating=safe&limit=1&tag=8",
-    "all": "https://api.nekosapi.com/v3/images/random?limit=1&tag=8"
+    "": "https://api.nekosapi.com/v4/images/random?rating=safe&limit=1&tag=8",
+    "all": "https://api.nekosapi.com/v4/images/random?limit=1&tag=8"
 }
 BLAHAJ_IMG_DATA = [
     ["Blahaj!", "cb.jpg"],
